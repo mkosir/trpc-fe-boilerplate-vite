ccd713b2a621ef28dbb6ca81bfabe2ee21b1d168
mkosir <kosir.mar@gmail.com>
2024-02-04 18:32:12 +0100

    chore(deps): update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,13 @@ import { trpcApiBoilerplateClient, trpcApiBoilerplateClientProvider } from './co
 import { routeTree } from './routeTree.gen';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { RouterProvider, createHashHistory, createRouter } from '@tanstack/react-router';
 import ReactDOM from 'react-dom/client';
 
-const router = createRouter({ routeTree });
+// GitHub pages - use hash routing since server doesn't support rewrites to index.html for HTTP requests.
+const hashHistory = createHashHistory();
+
+const router = createRouter({ routeTree, history: hashHistory });
 
 declare module '@tanstack/react-router' {
   // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
