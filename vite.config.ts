c771704e0daa565e85e1c883603339859546a5c0
mkosir <kosir.mar@gmail.com>
2024-02-04 18:20:58 +0100

    chore(deps): update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ import react from '@vitejs/plugin-react-swc';
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   plugins: [tsconfigPaths(), react(), TanStackRouterVite()],
-  base: '/trpc-fe-boilerplate-vite', // GitHub pages path prefix
+  // base: '/trpc-fe-boilerplate-vite', // GitHub pages path prefix
 });
