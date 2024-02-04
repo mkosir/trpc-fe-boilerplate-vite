d10a8aa48591db8827b2598b0f994362889c83ca
mkosir <kosir.mar@gmail.com>
2024-02-04 14:35:47 +0100

    chore(deps): update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), react(), TanStackRouterVite()],
   base: '/trpc-fe-boilerplate-vite', // GitHub pages path prefix
 });
