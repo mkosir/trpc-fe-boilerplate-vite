a96407d27af2cd064c7c86033895643ca547f122
mkosir <kosir.mar@gmail.com>
2024-02-15 14:36:51 +0000

    Deploying to gh-pages from @ mkosir/trpc-fe-boilerplate-vite@a60bc730ff0ced239a2dd642dc981e30dfefe813 🚀

---
+++ assets/Loader-BSWVdmmT.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Bsed29Li.js";const a=()=>r.jsx("div",{className:"w inline-block h-5 w-5 animate-spin rounded-full border-[3px] border-current border-t-transparent text-blueSky-300",role:"status","aria-label":"loading",children:r.jsx("span",{className:"sr-only",children:"Loading..."})});export{a as L};
