c2f74ad833778f0ab6222f8b606017c141983bf2
mkosir <kosir.mar@gmail.com>
2024-09-25 18:25:15 +0200

    fix(deps): update dependencies

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -42,14 +42,23 @@ const IndexLazyRoute = IndexLazyImport.update({
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
     '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
       preLoaderRoute: typeof IndexLazyImport
       parentRoute: typeof rootRoute
     }
     '/batches': {
+      id: '/batches'
+      path: '/batches'
+      fullPath: '/batches'
       preLoaderRoute: typeof BatchesLazyImport
       parentRoute: typeof rootRoute
     }
     '/users': {
+      id: '/users'
+      path: '/users'
+      fullPath: '/users'
       preLoaderRoute: typeof UsersLazyImport
       parentRoute: typeof rootRoute
     }
@@ -58,10 +67,72 @@ declare module '@tanstack/react-router' {
 
 // Create and export the route tree
 
-export const routeTree = rootRoute.addChildren([
-  IndexLazyRoute,
-  BatchesLazyRoute,
-  UsersLazyRoute,
-])
+export interface FileRoutesByFullPath {
+  '/': typeof IndexLazyRoute
+  '/batches': typeof BatchesLazyRoute
+  '/users': typeof UsersLazyRoute
+}
+
+export interface FileRoutesByTo {
+  '/': typeof IndexLazyRoute
+  '/batches': typeof BatchesLazyRoute
+  '/users': typeof UsersLazyRoute
+}
+
+export interface FileRoutesById {
+  __root__: typeof rootRoute
+  '/': typeof IndexLazyRoute
+  '/batches': typeof BatchesLazyRoute
+  '/users': typeof UsersLazyRoute
+}
+
+export interface FileRouteTypes {
+  fileRoutesByFullPath: FileRoutesByFullPath
+  fullPaths: '/' | '/batches' | '/users'
+  fileRoutesByTo: FileRoutesByTo
+  to: '/' | '/batches' | '/users'
+  id: '__root__' | '/' | '/batches' | '/users'
+  fileRoutesById: FileRoutesById
+}
+
+export interface RootRouteChildren {
+  IndexLazyRoute: typeof IndexLazyRoute
+  BatchesLazyRoute: typeof BatchesLazyRoute
+  UsersLazyRoute: typeof UsersLazyRoute
+}
+
+const rootRouteChildren: RootRouteChildren = {
+  IndexLazyRoute: IndexLazyRoute,
+  BatchesLazyRoute: BatchesLazyRoute,
+  UsersLazyRoute: UsersLazyRoute,
+}
+
+export const routeTree = rootRoute
+  ._addFileChildren(rootRouteChildren)
+  ._addFileTypes<FileRouteTypes>()
 
 /* prettier-ignore-end */
+
+/* ROUTE_MANIFEST_START
+{
+  "routes": {
+    "__root__": {
+      "filePath": "__root.tsx",
+      "children": [
+        "/",
+        "/batches",
+        "/users"
+      ]
+    },
+    "/": {
+      "filePath": "index.lazy.tsx"
+    },
+    "/batches": {
+      "filePath": "batches.lazy.tsx"
+    },
+    "/users": {
+      "filePath": "users.lazy.tsx"
+    }
+  }
+}
+ROUTE_MANIFEST_END */
