8fd0b8423e59c66d7924f9fb42fb2ea9003aa9ae
mkosir <kosir.mar@gmail.com>
2025-06-13 22:06:49 +0200

    fix(deps): update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,15 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), tanstackRouter(), tailwindcss()],
+  plugins: [
+    tsconfigPaths(),
+    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
   base: '/trpc-fe-boilerplate-vite', // GitHub pages path prefix
 });
