d3cbf366b0734e0651deb33e176a5fc9ae05f49a
mkosir <kosir.mar@gmail.com>
2025-06-13 22:05:08 +0200

    fix(deps): update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import { tanstackRouter } from '@tanstack/router-vite-plugin';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
