f735e309a2ca6ca26f94f3f4d677a2c4f867a3f5
mkosir <kosir.mar@gmail.com>
2025-06-15 08:33:23 +0200

    fix(deps): update dependencies

--- src/routes/users.tsx
+++ src/routes/users.tsx
@@ -1,7 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
 
-import { UsersPage } from 'modules';
+import { Loader } from '../common/components';
+import { trpc } from '../common/trpc-api-boilerplate';
+import { UserCard } from '../modules/UsersPage/UserCard';
 
 export const Route = createFileRoute('/users')({
-  component: UsersPage,
+  component: UsersComponent,
 });
+
+function UsersComponent() {
+  const { data: users } = useQuery(trpc.user.list.queryOptions());
+
+  return (
+    <div>
+      <h3>👤 Users</h3>
+      <div className="mt-4 flex flex-wrap">
+        {!users ? <Loader /> : users.map((user) => <UserCard key={user.id} user={user} />)}
+      </div>
+    </div>
+  );
+}
